c02b488276679ec36b663a198c7dbdf7c9419c43
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2021-06-05 01:12:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function myfunc() {
+  document.getElementById("doctor1").style.display = "block";
+  document.getElementById("student1").style.display = "none";
+  document.getElementById("others1").style.display = "none";
+}
+function myfunc1() {
+  document.getElementById("doctor1").style.display = "none";
+  document.getElementById("student1").style.display = "block";
+  document.getElementById("others1").style.display = "none";
+}
+function myfunc2() {
+  document.getElementById("doctor1").style.display = "none";
+  document.getElementById("student1").style.display = "none";
+  document.getElementById("others1").style.display = "block";
+}
